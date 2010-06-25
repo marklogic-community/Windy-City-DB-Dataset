498e50fb7965b6c793590911d7cd7238489cc6b0
Eric Bloch <eric.bloch@gmail.com>
2010-06-25 00:22:37 -0700

    Adding README and updating load script

--- marklogic/load-tools/load.sh
+++ marklogic/load-tools/load.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
+echo hello
+
 #
+if [ ! -f './load.sh' ] ; then
+   echo "Make sure current working directory is where load.sh exists";
+   exit -1;
+fi
 
 CP=recordloader.jar
 CP=$CP:xcc.jar
 CP=$CP:xpp3-1.1.4c.jar
 
-java -Xmx256m -cp $CP com.marklogic.ps.RecordLoader recordloader.properties
+US=admin
+PASS='Windy2010!'
+PORT=$1
 
-# end recordloader.sh
+echo Port is $1
+
+CONNECTION_STRING=xcc://$US:$PASS@localhost:$PORT
+java -Xmx256m \
+    -cp $CP \
+    -DCONNECTION_STRING=$CONNECTION_STRING \
+    -DINPUT_PATH=../marklogic-data \
+    com.marklogic.ps.RecordLoader recordloader.properties 
+
